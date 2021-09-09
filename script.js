54ac8f0d94da5363cdfe57b4eac68be96e8ff35f
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-09 13:19:12 -0400

    create wins, losses, and ties variable and fixed result output

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 var start = window.confirm("Would you like to play Rock, Paper, Scissors");
 
+function game() {
+    
+}
+
 if (start) {
     var choice = window.prompt("Enter 'R' for rock, 'P' for paper, or 'S' for scissors");
 }
 
 if (choice === "R" || choice === "r") {
     window.alert("You chose Rock");
+    choice = "R";
 } else if (choice === "P" || choice === "p") {
     window.alert("You chose Paper");
+    choice = "P";
 } else if (choice === "S" || choice === "s") {
     window.alert("You chose Scissors");
+    choice = "S";
 } else {
     window.alert("Please enter only R, S or P.")
 }
@@ -34,11 +41,27 @@ if (getNumber() === 0) {
 
 window.alert("The computer chose " + computerChoice);
 
+var wins;
+var losses;
+var ties;
+
 if (getChoice === choice) {
     window.alert("The result is a tie")
+    ties++;
 } else if ( (getChoice === "R" && choice === "S") || (getChoice === "P" && choice === "R") || (getChoice === "S" && choice === "P")) {
     window.alert("You lose")
+    losses++;
 } else {
     window.alert("You win!")
+    wins++;
 }
 
+var playAgain = confirm("Would you like to play again");
+
+if (playAgain) {
+    
+}
+
+
+
+//As a user, I want to see my total wins, ties, and losses after each round.
